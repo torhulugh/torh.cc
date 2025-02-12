a9db86138dc020475f4e2223072238e9406fc021
Tor Hulugh <torhulugh@gmail.com>
2025-02-12 00:02:08 -0500

    my works page edit

---
+++ notes.js
@@ -0,0 +1,34 @@
+var numberS = [ 2, 5, 7, 8, ];
+//after creating my array I created an empty array(tor) so I can push my unbers to
+var tor = "";
+
+// second i created a loop for the array used .lenght to stay loop through what ever number of items are added to the array
+//third i used I used a .push function to push the data i desired into an array(in the perimerters a userd a slice function to focus on the part of the arry data i wanted if not it would just ouput the bulk same data the number of time = to the number of items in the array because of ().lenght), but slice, plus the range will individually pase each item in the array and push it to the emty array(tor)
+// so remove the nan erro or message i add -1 to the .lenght in the loop which mad it (i < array.lenght-1)
+
+for(var i = 0; i <= numberS.length - 1; i++ + sumIt.push){
+  tor.push( numberS.slice(0,4)[i])
+}
+alert(tor)
+
+//for strings we push the item summed up or concertinated in an emty varable
+var numberS = [ 2, 5, 7, 8, ];
+var sumIt = 0
+
+for(var i = 0; i <= numberS.length - 1; i++){
+  sumIt += numberS.slice(0,4)[i] ;
+}
+console.log(sumIt)
+
+var variable = "";
+//coverts array to a variable
+for(var i = 0; i < 1; i++){
+	variable += cartItems.slice(0,slicethreshold);
+	console.log(sumOfCartItemsPrices)}
+
+//converts a variable to an array
+     var tor = []
+    for(var i = 0; i <= numberS.length - 1; i++ + sumIt.push){
+        tor.push( variable)
+      }
+      alert(tor)
